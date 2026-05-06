040f2808bef6d7537d241140b389b265b47e2891
Ilhan Hashi <I.saidhashi@gmail.com>
2026-05-05 22:25:11 -0500

    Rename uspGetInvoiceByVendor.sql to StoredProc/uspGetInvoiceByVendor.sql

---
+++ StoredProc/uspGetInvoiceByVendor.sql
@@ -0,0 +1,28 @@
+﻿USE [AP]
+GO
+/****** Object:  StoredProcedure [dbo].[uspGetInvoiceByVendor]    Script Date: 5/5/2026 10:17:33 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+ALTER PROCEDURE [dbo].[uspGetInvoiceByVendor]
+	@VendorID INT = NULL
+AS
+BEGIN 
+	SELECT
+		v.VendorID,
+		v.VendorName,
+		i.InvoiceID,
+		i.InvoiceNumber,
+		i.InvoiceDate,
+		i.InvoiceTotal,
+		i.InvoiceDueDate,
+		i.PaymentDate,
+		i.PaymentTotal,
+		i.CreditTotal
+	FROM Invoices i 
+	JOIN Vendors v ON i.VendorID = v.VendorID 
+	WHERE @VendorID IS NULL OR v.VendorID = @VendorID
+
+END;
